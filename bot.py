280cf88e2780f7ddac35d739fddd6c8fb6364b70
ilmir <ilmir531@gmail.com>
2023-01-05 12:55:51 +0300

    Initial commit

---
+++ bot.py
@@ -0,0 +1,245 @@
+import os
+from enum import Enum, auto
+from functools import partial
+
+from dotenv import load_dotenv
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup
+from telegram import ParseMode
+from telegram.ext import Updater, CommandHandler, ConversationHandler, CallbackQueryHandler, MessageHandler, Filters
+from validate_email import validate_email
+
+from moltin import get_product_info, get_file_info, get_cart_items, remove_cart_item, add_product_to_cart, \
+    get_item_id_in_cart, get_total_number_of_products, create_customers
+
+
+class Handlers(Enum):
+    HANDLE_DESCRIPTION = auto()
+    HANDLE_CART = auto()
+    HANDLE_BACKET = auto()
+    WAITING_mail = auto()
+
+
+def show_products(token, update, context):
+    query = update.callback_query
+    query.answer()
+    querydata = query.data
+    product_name, product_description, photo_id = get_product_info(token, query.data)
+    file_url = get_file_info(token, photo_id)
+    text = f'''
+    *{product_name}*
+*{product_description}*
+                    '''
+
+    keyboard = [
+        [InlineKeyboardButton("1кг", callback_data=f"1 {querydata}"),
+         InlineKeyboardButton("5кг", callback_data=f"5 {querydata}"),
+         InlineKeyboardButton("10кг", callback_data=f"10 {querydata}"),
+         ],
+        [InlineKeyboardButton("Корзина", callback_data='Корзина')],
+        [InlineKeyboardButton("Назад", callback_data='Назад')],
+
+    ]
+
+    reply_markup = InlineKeyboardMarkup(keyboard)
+    context.bot.send_photo(chat_id=query.message.chat_id, photo=file_url, caption=text, reply_markup=reply_markup,
+                           parse_mode=ParseMode.MARKDOWN_V2)
+
+    context.bot.delete_message(chat_id=update.callback_query.message.chat_id,
+                               message_id=update.callback_query.message.message_id,
+                               )
+    return Handlers.HANDLE_CART
+
+
+def next_list(token, update, context):
+    query = update.callback_query
+    query.answer()
+    total_number_of_products = get_total_number_of_products(token)
+    keyboard = [[InlineKeyboardButton("Karp", callback_data='80ddddcf-74c1-432f-bf51-8d5c07106772'),
+                 InlineKeyboardButton("Osetr", callback_data='caa3541a-c23a-4bc2-a446-0657a1a30fe9')],
+                [InlineKeyboardButton("Keta", callback_data='89f79f53-4d70-4588-89b7-d27e54908696'),
+                 InlineKeyboardButton("Grey Shark", callback_data='c9fe64c4-aad6-4397-9c8a-a300832ba418')],
+                [InlineKeyboardButton("Назад", callback_data='Назад')],
+                [InlineKeyboardButton(f"Корзина.Кол-во продуктов в корзине {total_number_of_products}",
+                                      callback_data='Корзина')]
+                ]
+
+    reply_markup = InlineKeyboardMarkup(keyboard)
+    context.bot.delete_message(chat_id=update.callback_query.message.chat_id,
+                               message_id=update.callback_query.message.message_id, )
+    context.bot.send_message(
+        chat_id=update.effective_chat.id,
+        text='Выбирите товар',
+        reply_markup=reply_markup,
+    )
+    return Handlers.HANDLE_DESCRIPTION
+
+
+def add_to_basket(token, update, context):
+    query = update.callback_query
+    query.answer(text='Товар добавлен в корзину')
+    split_querydata = query.data.split(" ")
+    product_id = split_querydata[1]
+    amount = int(split_querydata[0])
+    add_product_to_cart(token, product_id, amount)
+
+
+def show_bucket(token, update, context):
+    query = update.callback_query
+    query.answer()
+    keyboard = [[InlineKeyboardButton("Удалить Forel", callback_data='ed2b8fa9-5473-45a9-b52e-a1d718fa3002'),
+                 InlineKeyboardButton("Удалить Gorbusha", callback_data='d7451b26-94ad-407b-bade-c2a1a970b79a')],
+                [InlineKeyboardButton("Удалить Semga", callback_data='a8deb76c-0870-4156-8383-7c751abcf549'),
+                 InlineKeyboardButton("Удалить Sazan", callback_data='ff2de4f0-58db-4b34-b6a6-40283a77387c')],
+                [InlineKeyboardButton("Удалить Karp", callback_data='80ddddcf-74c1-432f-bf51-8d5c07106772'),
+                 InlineKeyboardButton("Удалить Osetr", callback_data='caa3541a-c23a-4bc2-a446-0657a1a30fe9')],
+                [InlineKeyboardButton("Удалить Keta", callback_data='89f79f53-4d70-4588-89b7-d27e54908696'),
+                 InlineKeyboardButton("Удалить Grey Shark", callback_data='c9fe64c4-aad6-4397-9c8a-a300832ba418')],
+                [InlineKeyboardButton("Оплатить", callback_data='Оплатить')],
+                [InlineKeyboardButton("В меню", callback_data='В меню')],
+                ]
+    reply_markup = InlineKeyboardMarkup(keyboard)
+    cart_items = get_cart_items(token)
+    context.bot.delete_message(chat_id=update.callback_query.message.chat_id,
+                               message_id=update.callback_query.message.message_id, )
+    context.bot.send_message(
+        chat_id=update.effective_chat.id,
+        text=''.join(cart_items),
+        reply_markup=reply_markup,
+    )
+    return Handlers.HANDLE_BACKET
+
+
+def remove_item_in_cart(token, update, context):
+    query = update.callback_query
+    product_id = query.data
+    query.answer(text='Продукт удален из корзины')
+    product_id_in_cart = get_item_id_in_cart(token, product_id)
+    remove_cart_item(token, product_id_in_cart)
+    return show_bucket(token, update, context)
+
+
+def first_page_of_products(token, update, context):
+    total_number_of_products = get_total_number_of_products(token)
+    query = update.callback_query
+    query.answer()
+    keyboard = [[InlineKeyboardButton("Forel", callback_data='ed2b8fa9-5473-45a9-b52e-a1d718fa3002'),
+                 InlineKeyboardButton("Gorbusha", callback_data='d7451b26-94ad-407b-bade-c2a1a970b79a')],
+                [InlineKeyboardButton("Semga", callback_data='a8deb76c-0870-4156-8383-7c751abcf549'),
+                 InlineKeyboardButton("Sazan", callback_data='ff2de4f0-58db-4b34-b6a6-40283a77387c')],
+                [InlineKeyboardButton("Вперед", callback_data='Вперед')],
+                [InlineKeyboardButton(f"Корзина. Кол-во продуктов в корзине {total_number_of_products}",
+                                      callback_data='Корзина')]
+                ]
+
+    reply_markup = InlineKeyboardMarkup(keyboard)
+    context.bot.delete_message(chat_id=update.callback_query.message.chat_id,
+                               message_id=update.callback_query.message.message_id, )
+    context.bot.send_message(
+        chat_id=update.effective_chat.id,
+        text='Выберите товар',
+        reply_markup=reply_markup,
+    )
+    return Handlers.HANDLE_DESCRIPTION
+
+
+def start(total_number_of_products, update, context, ):
+    keyboard = [[InlineKeyboardButton("Forel", callback_data='ed2b8fa9-5473-45a9-b52e-a1d718fa3002'),
+                 InlineKeyboardButton("Gorbusha", callback_data='d7451b26-94ad-407b-bade-c2a1a970b79a')],
+                [InlineKeyboardButton("Semga", callback_data='a8deb76c-0870-4156-8383-7c751abcf549'),
+                 InlineKeyboardButton("Sazan", callback_data='ff2de4f0-58db-4b34-b6a6-40283a77387c')],
+                [InlineKeyboardButton("Вперед", callback_data='Вперед')],
+                [InlineKeyboardButton(f"Корзина. Кол-во продуктов в корзине {total_number_of_products}",
+                                      callback_data='Корзина')]
+                ]
+
+    reply_markup = InlineKeyboardMarkup(keyboard)
+    context.bot.send_message(
+        chat_id=update.effective_chat.id,
+        text='Выберите товар',
+        reply_markup=reply_markup,
+    )
+    return Handlers.HANDLE_DESCRIPTION
+
+
+def get_email(update, context):
+    query = update.callback_query
+    query.answer()
+    context.bot.send_message(
+        chat_id=update.effective_chat.id,
+        text='Введите свою электронную почту'
+    )
+    return Handlers.WAITING_mail
+
+
+def wait_email(token, update, context):
+    keyboard = [InlineKeyboardButton("В меню", callback_data='Назад')],
+    reply_markup = InlineKeyboardMarkup(keyboard)
+    is_valid = validate_email(update.message.text)
+    if is_valid:
+        text = f'Вы отправили эту {update.message.text} почту '
+        context.bot.send_message(
+            chat_id=update.effective_chat.id,
+            text=text,
+            reply_markup=reply_markup
+        )
+        first_name = update.message.from_user.first_name
+        last_name = update.message.from_user.last_name if update.message.from_user.last_name else update.message.from_user.first_name
+        user_email = update.message.text
+        create_customers(token, first_name, last_name, user_email)
+
+        return Handlers.HANDLE_DESCRIPTION
+    else:
+        context.bot.send_message(
+            chat_id=update.effective_chat.id,
+            text='вы указали некорректную почту', )
+        return Handlers.WAITING_mail
+
+
+def cancel(update, context):
+    update.message.reply_text('Всего хорошего!.')
+    return ConversationHandler.END
+
+
+def main():
+    load_dotenv()
+    tg_token = os.getenv("TG_TOKEN")
+    moltin_api_key = os.getenv('MOLTIN_API_KEY')
+    updater = Updater(token=tg_token, use_context=True)
+    total_number_of_products = get_total_number_of_products(moltin_api_key)
+    conv_handler = ConversationHandler(
+        entry_points=[CommandHandler('start', partial(start, total_number_of_products))],
+
+        states={
+
+            Handlers.HANDLE_DESCRIPTION: [
+                CallbackQueryHandler(partial(next_list, moltin_api_key), pattern='^' + 'Вперед' + '$'),
+                CallbackQueryHandler(partial(first_page_of_products, moltin_api_key), pattern='^' + 'Назад' + '$'),
+                CallbackQueryHandler(partial(show_bucket, moltin_api_key, ), pattern='^' + 'Корзина' + '$'),
+                CallbackQueryHandler(partial(show_products, moltin_api_key)),
+
+            ],
+            Handlers.HANDLE_CART: [
+                CallbackQueryHandler(partial(first_page_of_products, moltin_api_key), pattern='^' + 'Назад' + '$'),
+                CallbackQueryHandler(partial(show_bucket, moltin_api_key, ), pattern='^' + 'Корзина' + '$'),
+                CallbackQueryHandler(partial(add_to_basket, moltin_api_key, )),
+            ],
+            Handlers.HANDLE_BACKET: [
+                CallbackQueryHandler(partial(first_page_of_products, moltin_api_key), pattern='^' + 'В меню' + '$'),
+                CallbackQueryHandler(get_email, pattern='^' + 'Оплатить' + '$'),
+                CallbackQueryHandler(partial(remove_item_in_cart, moltin_api_key)),
+
+            ],
+            Handlers.WAITING_mail: [
+                MessageHandler(Filters.text, partial(wait_email, moltin_api_key))
+            ]
+        },
+        fallbacks=[CommandHandler('cancel', cancel)]
+    )
+    updater.dispatcher.add_handler(conv_handler)
+
+    updater.start_polling()
+    updater.idle()
+
+
+if __name__ == '__main__':
+    main()
---
+++ moltin.py
@@ -0,0 +1,208 @@
+import requests
+from dotenv import load_dotenv
+
+
+def get_total_number_of_products(api_token):
+    ''' Возвращает количество продуктов в крозине'''
+    headers = {
+        'Authorization': api_token,
+    }
+
+    response = requests.get('https://api.moltin.com/v2/carts/korzinka/items', headers=headers)
+    response.raise_for_status()
+    decode_response = response.json()['data']
+    products_in_the_cart = len(decode_response)
+    return products_in_the_cart
+
+
+def get_cart_items(api_token):
+    ''' Возвращает текст для функции show_bucket() '''
+    headers = {
+        'Authorization': api_token,
+    }
+
+    response = requests.get('https://api.moltin.com/v2/carts/korzinka/items', headers=headers)
+    response.raise_for_status()
+    decode_response = response.json()['data']
+
+    text = [f'''
+{item['name']}
+{item['description']}\n\n'''
+            for item in decode_response]
+    text.append(get_amount(api_token))
+    return text
+
+
+def add_product_to_cart(api_token, product_id, amount):
+    '''Добавляет продукты в корзину'''
+    headers = {
+        'Authorization': api_token,
+        'Content-Type': 'application/json',
+
+    }
+
+    data = {
+        'data': {
+            'id': product_id,
+            'type': 'cart_item',
+            "quantity": amount,
+        }
+    }
+
+    response = requests.post('https://api.moltin.com/v2/carts/korzinka/items',
+                             headers=headers,
+                             json=data)
+    return response.json()
+
+
+def get_item_id_in_cart(token, product_id):
+    ''' возвращает уникальный id продукта в корзине он нужен что бы удалить его потом из корзины'''
+    headers = {
+        'Authorization': token,
+    }
+    cart_items = requests.get('https://api.moltin.com/v2/carts/korzinka/items', headers=headers).json()['data']
+    for item in cart_items:
+        if item.get('product_id', None) == product_id:
+            return item['id']
+    return None
+
+
+def remove_cart_item(token, poduct_id):
+    ''' Удаляет продукт из коризны'''
+    headers = {
+        'Authorization': token,
+    }
+    response = requests.delete(f'https://api.moltin.com/v2/carts/korzinka/items/{poduct_id}',
+
+                               headers=headers)
+
+
+
+def create_cart(api):
+    ''' Создает корзину'''
+    headers = {
+        'Authorization': api,
+        'Content-Type': 'application/json',
+        'x-moltin-customer-token': 'eyJhbGciOiAiSFMyNTYiLCAidHlwIjogIkpXVCJ9.eyJzdWIiOiI1NmJlOWU5Ny00OTMxLTQwOGUtYWU5Ni1kODc1YzU0ZDc5N2MiLCJuYW1lIjoidGVzdCB0ZXN0IiwiZXhwIjoxNjcxMjczMjQ3LCJpYXQiOjE2NzExODY4NDcsImp0aSI6IjkyNWM0YjA2LWNiMmItNDY0ZS04ZDU5LWI2NjQ1OGFiYmRmNSJ9.7b0d8898c63bce6e1425480cfb52dff912ddda63a8230344d4d3e51cf6d89ded'
+    }
+    body = {
+        'data': {
+            'name': 'korzinka',
+
+        },
+    }
+    response = requests.post('https://api.moltin.com/v2/carts', headers=headers, json=body)
+
+
+
+def get_token_client_credential_token():
+    ''' функция для получения API токена '''
+    data = {
+        'client_id': '4mxJCYs8TpHP19WdqaUBaOsy5BPdhOkrEr9LVjki1S',
+        'client_secret': 'HjRWvMI8kKBJ5ikxY4BwuxALXxHtvYEZrUrbRzvE7J',
+        'grant_type': 'client_credentials',
+    }
+
+    response = requests.post('https://api.moltin.com/oauth/access_token', data=data).json()
+    print(response['access_token'])
+
+
+def get_cutomers(api):
+    ''' Получить информацию о покупателях '''
+    headers = {
+        'Authorization': api,
+
+    }
+
+    response = requests.get('https://api.moltin.com/v2/customers', headers=headers).json()
+
+    return response['data'][0]['id']
+
+
+def customers_token(api):
+    ''' Получить токен покупателя'''
+    headers = {
+        'Authorization': api,
+
+    }
+
+    json_data = {
+        'data': {
+            'type': 'token',
+            'email': 'test@swanson.com',
+            'password': 'mysecretpassword',
+            "authentication_mechanism": "password"
+
+        },
+    }
+
+    response = requests.post('https://api.moltin.com/v2/customers/tokens', headers=headers, json=json_data)
+
+
+
+def get_product_info(token, product_id):
+    ''' Информация о продукте '''
+    headers = {
+        'Authorization': token,
+    }
+
+    response = requests.get(f'https://api.moltin.com/pcm/products/{product_id}', headers=headers)
+    response.raise_for_status()
+    decode_response = response.json()
+    product_name = decode_response['data']['attributes']['name']
+    product_description = decode_response['data']['attributes']['description']
+    photo_id = decode_response['data']['relationships']['main_image']['data']['id']
+    return product_name, product_description, photo_id
+
+
+def get_file_info(token, photo_id):
+    ''' Получить ссылку на фото продукта'''
+    headers = {
+        'Authorization': token,
+    }
+
+    response = requests.get(f'https://api.moltin.com/v2/files/{photo_id}', headers=headers)
+    response.raise_for_status()
+    decode_response = response.json()
+    file_url = decode_response['data']['link']['href']
+    return file_url
+
+
+def create_customers(token, firstname, lastname, email):
+    ''' Создать покупателя'''
+    headers = {
+        'Authorization': token,
+        'Content-Type': 'application/json',
+    }
+
+    json_data = {
+        'data': {
+            'type': 'customer',
+            'name': f'{firstname} {lastname}',
+            'email': f'{email}',
+            'password': 'mysecretpassword',
+        },
+    }
+
+    response = requests.post('https://api.moltin.com/v2/customers', headers=headers, json=json_data)
+
+
+def get_amount(token):
+    ''' возвращает общую сумму коризны'''
+    headers = {
+        'Authorization': token,
+    }
+
+    response = requests.get("https://api.moltin.com/v2/carts/korzinka/", headers=headers)
+    response.raise_for_status
+    decode_response = response.json()
+    return decode_response['data']['meta']['display_price']['with_tax']['formatted']
+
+
+def main():
+    load_dotenv()
+    print(get_token_client_credential_token())
+
+
+if __name__ == '__main__':
+    main()
